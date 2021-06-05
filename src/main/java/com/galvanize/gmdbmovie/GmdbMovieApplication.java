7d95a9b57d19659a7dd97a33baba23ab5a8a5a76
Padmashree Ashokkumar <padhu2192@gmail.com>
2021-06-05 11:15:12 -0400

    Initial Commits

---
+++ src/main/java/com/galvanize/gmdbmovie/GmdbMovieApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.gmdbmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbMovieApplication.class, args);
+	}
+
+}
